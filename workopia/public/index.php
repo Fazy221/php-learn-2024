aa5ca383ab5e7cdc1fc57f8f246e25f3384b2c6e
Aqib Malik <hmaqibmalik.yonasi@gmail.com>
2024-07-03 17:00:16 +0500

    seperating routing

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -1,15 +1,5 @@
 <?php 
 require '../helper.php';
-$routers = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/errors/404.php'
-];
 
 $uri = $_SERVER['REQUEST_URI'];
-if(array_key_exists($uri, $routers)) {
-    require(basePath($routers[$uri]));
-} else {
-    require basePath($routers['404']);
-}
+require basePath('router.php');
