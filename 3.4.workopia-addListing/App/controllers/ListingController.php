3f3e5b8c45134c51a1ab1da1fd8ad6cc27589bf1
Fazy221 <faizanraza2221@gmail.com>
2024-08-01 19:27:07 +0500

    add listing

--- 3.4.workopia-addListing/App/controllers/ListingController.php
+++ 3.4.workopia-addListing/App/controllers/ListingController.php
@@ -60,21 +60,36 @@ class ListingController
                 'listing' => $newListingData
             ]);
         } else {
+            // echo 'success';
+            // As we'll only want to insert keys which have data, will have to do dynamically otherwise it'll look like below
+            // $this->db->query('INSERT INTO listings (title, description, salary, tags, company, address, city, state, phone, email, requirements, benefits, user_id) VALUES (:title, :description, :salary, :tags, :company, :address, :city, :state, :phone, :email, :requirements, :benefits, :user_id)');
+            // Will make empty field arr first then loop through $newListingData to get key and value and add key to field arr
             $fields = [];
             foreach ($newListingData as $key => $value) {
                 $fields[] = $key;
             }
+            // inspectAndDie($fields); // will give us result like [1=>'title', 2=>'description']
+            // Will now turn arr into string, seperated by space and comma
             $fields = implode(', ', $fields);
+            // inspectAndDie($fields); // will give us result like: string(104) "title, description, salary, requirements, benefits, company, address, city, state, phone, email, user_id"
+
+            // Will do same thing for values
             $values = [];
             foreach ($newListingData as $key => $value) {
+                // will convert empty strings to null so they can easily get inserted into db that way
                 if ($value === '') {
                     $newListingData[$key] = null;
                 };
-                $values[] = ':' . $key; 
+                $values[] = ':' . $key; // will look like [':description', ':requirement']
             }
             $values = implode(', ', $values);
+            // inspectAndDie($values); // will look like this: string(116) ":title, :description, :salary, :requirements, :benefits, :company, :address, :city, :state, :phone, :email, :user_id"
+
+            // Now will write query
             $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
             $this->db->query($query, $newListingData);
+
+            // Once data is inserted then can redirect to listing page. Already made helper func in it
             redirect('/listings');
         }
     }
---
+++ 3.4.workopia-addListing/changes.md
@@ -0,0 +1,4 @@
+## Wrote whole code to dynamically insert data into db instead of manually writing each key and value into query 
+## Writing helper func redirect once work is executed
+## Adding tags field since previously forgot in create.view.php then if empty, don't show tag block in index.view.php in views/listing. same added in home view and listings/show.view.php
+## Added salary to required fields by simply writing 'salary' in requiredFields arr
